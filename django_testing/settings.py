d5efacd32306a80844a79b78fe0b36e6142720fe
heyma <heymaker@yandex.ru>
2022-09-04 18:49:53 +0500

    final version

--- django_testing/settings.py
+++ django_testing/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "jkglh;mou;ljiowyvnu91pot49uq0wotjqirtvqomt"
+SECRET_KEY = "jkglh;mou;ljiowyvnu91pot4lhkj9uq0wotjqirtvqomt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
